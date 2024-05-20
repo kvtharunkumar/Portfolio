1785511b521a9951e946034b4777d0be743fe4fd
tharun <kvtharunkumar99@gmial.com>
2024-05-20 23:10:17 +0530

    dploy

--- src/views/resume.vue
+++ src/views/resume.vue
@@ -5,7 +5,7 @@
             <div class="flex flex-col lg:flex-row items-center space-y-5 lg:space-y-0 lg:space-x-10">
                 <p class="text-white font-bold text-5xl">Download</p>
             
-                    <a href="https://drive.google.com/file/d/1WV354qtbqQpuQvM0AkZH_Z3qifU0_7V5/view?usp=drive_link"> <button class="bg-[#ee7534] dark:bg-green-500 text-white px-10 py-4 rounded-full text-lg font-bold hover:cursor-pointer">
+                    <a href="https://drive.google.com/file/d/1NGu5SesICwlNKLivgk9Se33cz7sERxVg/view?usp=drive_link"> <button class="bg-[#ee7534] dark:bg-green-500 text-white px-10 py-4 rounded-full text-lg font-bold hover:cursor-pointer">
                         Resume
                     </button>
                 </a>
